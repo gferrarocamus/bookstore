07820013e05d88bb4827a0fe318928a5cb1b74da
Zeha Irawan <jangkaringbumi@gmail.com>
2019-07-31 22:29:45 +0700

    Create empty components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import BookList from '../src/components/BookList';
+import BookForm from '../src/components/BookForm';
 
 function App() {
   return (
-
+    <div>
+      <BookList />
+      <BookForm />
+    </div>
   );
 }
 
