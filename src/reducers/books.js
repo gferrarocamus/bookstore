f4b6f7d06142c69a95b52709f11dc51ac31e09a7
Zeha Irawan <jangkaringbumi@gmail.com>
2019-08-05 16:57:13 +0700

    fix some stuff

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,18 +1,37 @@
-const books = (state = [], action) => {
+const initialState = [
+  {
+    id: 1000,
+    title: 'The Great Gatsby',
+    category: 'Action',
+  },
+  {
+    id: 1001,
+    title: 'The Grapes of Wrath',
+    category: 'Action',
+  },
+  {
+    id: 1002,
+    title: 'Nineteen Eighty-Four',
+    category: 'History',
+  },
+];
+
+const books = (state = initialState, action) => {
   const newState = [...state];
   switch (action.type) {
     case 'CREATE_BOOK':
       newState.push(action.book);
       return newState;
     case 'REMOVE_BOOK': {
-      const index = newState.reduce((acc, current, i) => {
-        if (current.id === action.id) {
-          acc = i;
-        }
-        return acc;
-      }, 0);
-      newState.splice(index, 1);
-      return newState;
+      return state.filter(({ id }) => action.id !== id);
+      // const index = newState.reduce((acc, current, i) => {
+      //   if (current.id === action.id) {
+      //     acc = i;
+      //   }
+      //   return acc;
+      // }, 0);
+      // newState.splice(index, 1);
+      // return newState;
     }
     default:
       return state;
--- src/store.js
+++ src/store.js
@@ -2,27 +2,8 @@ import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import mainReducer from './reducers/index';
 
-const initialState = {
-  books: [
-    {
-      id: 1000,
-      title: 'The Great Gatsby',
-      category: 'Action',
-    },
-    {
-      id: 1001,
-      title: 'The Grapes of Wrath',
-      category: 'Action',
-    },
-    {
-      id: 1002,
-      title: 'Nineteen Eighty-Four',
-      category: 'History',
-    },
-  ],
-  filter: '',
-};
+const middleWares = process.env.NODE_ENV === 'development' ? composeWithDevTools() : undefined;
 
-const store = createStore(mainReducer, initialState, composeWithDevTools());
+const store = createStore(mainReducer, middleWares);
 
 export default store;
